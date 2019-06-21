f69b752e0d48b604551c5f369b9e46311a45cb39
MeganPaffrath <meganpaffrath@gmail.com>
2019-06-20 21:14:10 -0700

    Renames both photos and videos - should account for errors

--- PhotoRenamer.py
+++ PhotoRenamer.py
@@ -11,28 +11,93 @@ import os, sys
 from PIL import Image
 from shutil import copyfile # copy images to new dest.
 	# copyfile(src, dst)
+# For Videos:
+	# Download crtime
+	# pip3 install ~/Downloads/crtime-0.0.6-py2.py3-none-any.whl
+from crtime import get_crtimes_in_dir
+import datetime
 
 
 def getDateAndTime(filePath):
-    return Image.open(filePath)._getexif()[36867]
+	return Image.open(filePath)._getexif()[36867]
+
+def getFileType(file):
+	return file.split(".")[-1]
 
 def moveRenameablesToDone(originalFile, newName):
 	oldDestination = "Rename/" + originalFile
 	newDestination = "Done/" + newName
 	copyfile(oldDestination, newDestination)
 
-def renameFilesFromRename():
+def newFileName(utcTime, file):
+	fileType = getFileType(file)
+	newName = utcTime[0:4] + "_" + utcTime[5:7] + "_" + utcTime[8:10] + "_at_" + utcTime[11:13] + "_" + utcTime[14:16] + "_" + utcTime[17:19] + "." + fileType
+	return newName
+
+def renamedAndMovedMSG(originalFile, newName):
+	print("\t" + newName + " -- was created from " + originalFile)
+
+def unkFileTypeCopy():
+	directoryFiles = os.listdir("Rename")
+	for file in directoryFiles: # For each file in Rename folder:
+		fileType = file.split(".")[-1] # get file type for each photo
+		if (fileType.lower() != "jpg" and fileType != "m4v" and fileType != "MOV" and fileType != "mp4"):
+			newName = "error" + file
+			moveRenameablesToDone(file, newName)
+			renamedAndMovedMSG(file, newName)
+
+
+def renamePhotos():
 	directoryFiles = os.listdir("Rename")
 	for file in directoryFiles: # For each file in Rename folder:
 		fileName = file # get individual file name
 		fileType = fileName.split(".")[-1] # get file type for each photo
 
-		if (fileType == "jpg" or fileType == "JPG"):
+		if (fileType.lower() == "jpg"):
 			photoPath = "Rename/" + file
-			date = getDateAndTime(photoPath)
-			print ("\t" + fileName + " - a " + fileType + " file -  was created: " + date )
-			newName = date[0:4] + "_" + date[5:7] + "_" + date[8:10] + "_at_" + date[11:13] + "_" + date[14:16] + "_" + date[17:19] + "." + fileType
-			print ("\t\tYour new file name is " + newName)
+			try:
+				utcTime = getDateAndTime(photoPath)
+				newName = newFileName(utcTime, file)
+			except KeyError:
+				newName = "error" + file
 			moveRenameablesToDone(file, newName)
+			renamedAndMovedMSG(file, newName)
+
+			
+
+def renameVideos():
+	for longFile, date in get_crtimes_in_dir("./Rename", raise_on_error=True, as_epoch=True):
+		file = longFile.split("./Rename/")[-1]
+		fileType = getFileType(file)
+		if (fileType == "m4v" or fileType == "MOV" or fileType == "mp4"):
+			epochTime = str(date)
+			utcTime = datetime.datetime.fromtimestamp(float(epochTime)).strftime('%Y-%m-%d %H:%M:%S')
+			newName = newFileName(utcTime, file)
+			moveRenameablesToDone(file, newName)
+			renamedAndMovedMSG(file, newName)	
+
+def renameFilesFromRename():
+	renamePhotos()
+	print("//////////////////////////////////////////////////////////////////////////////////////////////////////")
+	renameVideos()
+	print("//////////////////////////////////////////////////////////////////////////////////////////////////////")
+	unkFileTypeCopy()
+
+		# elif (fileType == "m4v"):
+		# 	videoPath = "Rename/" + file
+		# 	date = "none"
+		# 	# date = datetime.datetime.fromtimestamp(os.path.getcrtime(videoPath))
+		# 	# for file, date in get_crtimes_in_dir(".", raise_on_error=True, as_epoch=False):
+		# 	# 	print(file + "at" + str(date))			
+		# 	for file, date in get_crtimes_in_dir("./Rename", raise_on_error=True, as_epoch=True):
+		# 		print(file + "at" + str(date))
+
+
+			# date = get_crtimes_in_dir(videoPath)
+			# print (date)
+			# print ("\t" + fileName + " - a " + fileType + " file -  was created: " + date )
+			# newName = date[0:4] + "_" + date[5:7] + "_" + date[8:10] + "_at_" + date[11:13] + "_" + date[14:16] + "_" + date[17:19] + "." + fileType
+			# print ("\t\tYour new file name is " + newName)
+			# moveRenameablesToDone(file, newName)
 
 renameFilesFromRename()
--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # photoRenamer
 
+- Should rename: JPG, jpg, mp4, MOV
+- Files that don't get renamed get their file names changed to have "error" in the front.
+- Cannot rename: HEIC, PNG
+	- such files become: errorOLDNAME.ext
+
+
 1. Save py program in a file called "PhotoRenamer" (or something else if you wish)
 2. Within that folder make 2 folders: "Rename" and "Done"
 3. Put photos to rename in the "Rename" folder
 4. Run from terminal
 5. Make sure all of your files got renamed propperly in the Done folder
-6. Move files out of the folders to use again
+6. Move files out of both folders to use again
